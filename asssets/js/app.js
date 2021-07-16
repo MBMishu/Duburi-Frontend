e4f8f69f16174229a1c4389eab536d9d9d20fa61
MbMishu <masumbillah1717@gmail.com>
2021-07-17 05:54:58 +0600

    updated

--- asssets/js/app.js
+++ asssets/js/app.js
@@ -2,55 +2,55 @@
 !(function($) {
     "use strict";
     $(function() {
-        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="tooltip"]').tooltip();
     });
 
     // wow initiate
     var wow = new WOW({
-        animateClass: 'animated',
-        offset: 50
+        animateClass: "animated",
+        offset: 50,
     });
     wow.init();
 
     // Initiate venobox (lightbox feature used in portofilo)
-    $('.venobox').venobox({
-        spinner: 'cube-grid',
-        'share': false
+    $(".venobox").venobox({
+        spinner: "cube-grid",
+        share: false,
     });
 
     // owl carousel
-    if ($('.resume-list').length) {
-        $('.resume-list').owlCarousel({
+    if ($(".sponsor-list").length) {
+        $(".sponsor-list").owlCarousel({
             loop: true,
             nav: false,
-            dots: true,
+            dots: false,
             items: 3,
             margin: 30,
-            autoplay: false,
-            smartSpeed: 700,
+            autoplay: true,
+            smartSpeed: 1000,
             autoplayTimeout: 6000,
             responsive: {
                 0: {
-                    items: 1,
-                    margin: 0
+                    items: 3,
+                    margin: 0,
                 },
                 460: {
-                    items: 1,
-                    margin: 0
+                    items: 3,
+                    margin: 0,
                 },
                 576: {
-                    items: 2,
-                    margin: 20
+                    items: 3,
+                    margin: 20,
                 },
                 992: {
-                    items: 2,
-                    margin: 30
-                }
-            }
+                    items: 3,
+                    margin: 30,
+                },
+            },
         });
     }
-    if ($('.skill-list').length) {
-        $('.skill-list').owlCarousel({
+    if ($(".skill-list").length) {
+        $(".skill-list").owlCarousel({
             loop: true,
             nav: false,
             dots: true,
@@ -62,25 +62,25 @@
             responsive: {
                 0: {
                     items: 4,
-                    margin: 10
+                    margin: 10,
                 },
                 460: {
                     items: 4,
-                    margin: 10
+                    margin: 10,
                 },
                 576: {
                     items: 6,
-                    margin: 20
+                    margin: 20,
                 },
                 992: {
                     items: 6,
-                    margin: 30
-                }
-            }
+                    margin: 30,
+                },
+            },
         });
     }
-    if ($('.project-list').length) {
-        $('.project-list').owlCarousel({
+    if ($(".project-list").length) {
+        $(".project-list").owlCarousel({
             loop: false,
             nav: false,
             dots: true,
@@ -92,23 +92,21 @@
             responsive: {
                 0: {
                     items: 1,
-                    margin: 0
+                    margin: 0,
                 },
                 460: {
                     items: 1,
-                    margin: 0
+                    margin: 0,
                 },
                 576: {
                     items: 2,
-                    margin: 0
+                    margin: 0,
                 },
                 992: {
                     items: 4,
-                    margin: 0
-                }
-            }
+                    margin: 0,
+                },
+            },
         });
     }
-
-
 })(jQuery);
